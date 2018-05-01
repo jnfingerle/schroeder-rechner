b121edf310c6f6b2154e0a32c011a0a0dcfac671
Jan Niklas Fingerle <jan.niklas@fingerle.org>
2018-05-01 18:13:11 +0200

    SCF 2017/18

--- app/helpers/mitfahrer.js
+++ app/helpers/mitfahrer.js
@@ -4,22 +4,22 @@ export default [
     name: 'Vollpreiszahler',
     explanation: 'Chormitglied, Partner(in), anderer Erwachsener, '+
     'der aus mehr oder weniger eigenen Stücken mitfährt und voll zahlt',
-    sockel: 17,
-    nacht: 17
+    sockel: 23,
+    nacht: 23
   },
   {
     id: 2,
     name: 'Student',
     explanation: 'Wie Standard, aber Sondertarif für Studenten sowie nach Absprache mit Alina',
-    sockel: 12,
-    nacht: 12
+    sockel: 15,
+    nacht: 15
   },
   {
     id: 3,
     name: 'Kind',
     explanation: 'Kinder von Standard/Student-Mitfahrern und ggf. deren Freunde. Auch größere Kinder/Jugendliche zählen darunter.',
     sockel: 0,
-    nacht: 10
+    nacht: 11
   },
   {
     id: 4,
